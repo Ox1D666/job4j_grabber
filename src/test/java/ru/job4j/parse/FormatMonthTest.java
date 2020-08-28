6f0e0af15da8c7df6fefd0a20494fe3fef229cf7
Kirill Romanov <kirill.coldline@gmail.com>
2020-08-28 14:47:59 +0300

    7. Web. [#340970]

--- src/test/java/ru/job4j/parse/FormatMonthTest.java
+++ src/test/java/ru/job4j/parse/FormatMonthTest.java
@@ -1,15 +1,31 @@
 package ru.job4j.parse;
 
 import org.junit.Test;
+
+import java.text.DateFormatSymbols;
 import java.text.ParseException;
-import static org.hamcrest.Matchers.is;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import static org.junit.Assert.*;
 
 public class FormatMonthTest {
+    private final Locale language = new Locale("ru");
     @Test
     public void setDate() throws ParseException {
         FormatMonth fm = new FormatMonth();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yy, HH:mm");
+        DateFormatSymbols instance = DateFormatSymbols.getInstance(language);
+        instance.setShortMonths(new String[]{"янв", "февр", "мар", "апр", "май", "июн", "июл",
+                "авг", "сент", "окт", "нояб", "дек"});
+        dateFormat.setDateFormatSymbols(instance);
         String date = "17 окт 07, 14:42";
-        assertThat(fm.format(date).toString(), is("Wed Oct 17 14:42:00 MSD 2007"));
+        Date res = fm.format(date);
+        String[] str = dateFormat.format(res).split(" ");
+        assertEquals("17", str[0]);
+        assertEquals("окт", str[1]);
+        assertEquals("07,", str[2]);
+        assertEquals("14:42", str[3]);
     }
 }
